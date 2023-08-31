c41511320ca7427bdca8ed22f9a68942ae6c94d5
Jlvck <jonaruthan@gmail.com>
2023-08-31 02:31:19 +0100

    changing from listviewbuilder to listview 2

--- lib/widgets/hymn_view_widget.dart
+++ lib/widgets/hymn_view_widget.dart
@@ -11,23 +11,24 @@ class HymnViewWidget extends StatelessWidget {
       required this.hymnChorus,
       required this.isChorus});
 
-  Widget versesWidget(List<List<String>> verses, int index) {
-    return ListView.builder(
+  Widget versesWidget(List<String> verseNumber) {
+    return ListView(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
       primary: false,
-      itemBuilder: (ctxx, i) => Align(
-        alignment: Alignment.center,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            verses[index][i],
-            style: const TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
+      children: List.generate(verseNumber.length, (i) {
+        return Align(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              verseNumber[i],
+              style: const TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
           ),
-        ),
-      ),
-      itemCount: verses[index].length,
+        );
+      }),
     );
   }
 
@@ -44,25 +45,26 @@ class HymnViewWidget extends StatelessWidget {
                 fontSize: 20,
                 fontStyle: FontStyle.italic),
           ),
-          ListView.builder(
+          ListView(
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
             primary: false,
-            itemBuilder: (ctxx, t) => Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  chorus[t],
-                  style: const TextStyle(
-                      fontSize: 20, fontStyle: FontStyle.italic),
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                  maxLines: 2,
+            children: List.generate(chorus.length, (index) {
+              return Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    chorus[index],
+                    style: const TextStyle(
+                        fontSize: 20, fontStyle: FontStyle.italic),
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    maxLines: 2,
+                  ),
                 ),
-              ),
-            ),
-            itemCount: chorus.length,
+              );
+            }),
           ),
         ],
       ),
@@ -73,27 +75,30 @@ class HymnViewWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        ListView.builder(
+        ListView(
           shrinkWrap: true,
           physics: const ScrollPhysics(),
-          itemBuilder: (ctx, index) => Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Text(
-                    "${index + 1}",
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 30),
-                  ),
-                  versesWidget(hymnVerses, index),
-                  if (isChorus) chorusWidget(hymnChorus)
-                ],
+          children: List.generate(hymnVerses.length, (index) {
+            return Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "${index + 1}",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                    versesWidget(
+                      hymnVerses[index],
+                    ),
+                    if (isChorus) chorusWidget(hymnChorus)
+                  ],
+                ),
               ),
-            ),
-          ),
-          itemCount: hymnVerses.length,
+            );
+          }),
         ),
         const Padding(
           padding: EdgeInsets.all(20),
