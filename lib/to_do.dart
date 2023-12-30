5c560ccdd3565609a2b9dc489df7b28b52ecc11c
Jlvck <jonaruthan@gmail.com>
2023-12-30 08:54:04 +0100

    creating a TO DO File

---
+++ lib/to_do.dart
@@ -0,0 +1,22 @@
+//I JUST CREATED THIS FILE TO WRITE THINGS I NEED TO COMPLETE LATER ON 
+//IN THE PROJECT
+
+//1. Page_transitons for every pushing of pages
+
+//2. Grid View of hymns(a card design).. 
+//...for Landscape View Mode to have a responsive app....
+//....(present design looks ugly in tablet sized devices)
+
+//3. Constraint Size for maintext field
+
+//4. Complete 500 hymns with both lang and write more test...
+//...to check integrity of the hymns_data
+
+//5. I must write at least 2 widget_test.
+ 
+//6. The Repo for this project is a mess, I need to work on it...
+//...don't forget to add a license to the repo(Man must guide always....
+//....never loose guard).
+
+//7. Read on how to update apps on play console.
+
