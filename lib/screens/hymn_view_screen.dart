bb9696edd6d2eedd29214447a9226a0fd2b25dba
Jlvck <jonaruthangmail.com>
2024-11-05 08:01:22 +0100

    initial commit

--- lib/screens/hymn_view_screen.dart
+++ lib/screens/hymn_view_screen.dart
@@ -24,20 +24,54 @@ class HymnViewScreen extends StatefulWidget {
 
 class _HymnViewScreenState extends State<HymnViewScreen> {
   List<Hymn> hymnList = [];
-  int pagenumber = 0;
-
+  int pagenumber = 1;
+  late final PageController controller;
+  bool changePage = false;
   @override
   void initState() {
     hymnList = Provider.of<HymnBookProvider>(context, listen: false).hymnList;
-    pagenumber = int.parse(widget.id) - 1;
+
+    controller = PageController(
+      initialPage: pagenumber,
+    );
 
     super.initState();
+
+    controller.addListener(() {
+      if ((changePage && (pagenumber != 1 && pagenumber == controller.page)) ||
+          (controller.page == 2.0)) {
+        controller
+            .animateToPage(2,
+                curve: Curves.easeInOut, duration: Duration(milliseconds: 200))
+            .then(
+          (value) {
+            controller
+                .animateToPage(1,
+                    curve: Curves.easeInOut,
+                    duration: Duration(milliseconds: 200))
+                .then(
+              (value) {
+                setState(() {
+                  changePage = false;
+                  pagenumber = 1;
+                });
+              },
+            );
+          },
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final PageController controller = PageController(initialPage: pagenumber);
-
     return Semantics(
       label: "Hymn Number ${widget.id}",
       child: Scaffold(
@@ -77,20 +111,25 @@ class _HymnViewScreenState extends State<HymnViewScreen> {
         ),
         body: PageView(
           clipBehavior: Clip.hardEdge,
+          controller: controller,
           onPageChanged: (value) {
+            print("Current page $value");
             setState(() {
+              changePage = true;
               pagenumber = value;
             });
           },
-          controller: controller,
-          children: List.generate(hymnList.length, (index) {
-            return HymnViewWidget(
-              hymnYorubaVerses: hymnList[index].versesYoruba,
-              hymnYorubaChorus: hymnList[index].chorusYoruba,
-              hymnEnglishVerses: hymnList[index].versesEnglish,
-              hymnEnglishChorus: hymnList[index].chorusEnglish,
-            );
-          }),
+          physics: BouncingScrollPhysics(),
+          children: <Widget>[
+            Text("1"),
+            HymnViewWidget(
+              hymnYorubaVerses: hymnList[pagenumber + 1].versesYoruba,
+              hymnEnglishVerses: hymnList[pagenumber + 1].versesEnglish,
+              hymnEnglishChorus: hymnList[pagenumber + 1].chorusEnglish,
+              hymnYorubaChorus: hymnList[pagenumber + 1].chorusYoruba,
+            ),
+            Text("2")
+          ],
         ),
       ),
     );
